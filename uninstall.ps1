85d13a765a5886068860fd088aa4a3afbd842163
JC <jym@jym.sg>
2020-06-30 09:35:42 +0800

    Update uninstall.ps1

--- uninstall.ps1
+++ uninstall.ps1
@@ -19,37 +19,7 @@ Write-Host "Searching for existing OpenEDR (aka DataFusion)..."
 $app = Get-WmiObject Win32_Product -filter "Name='DataFusion'"
 if($app) { 
     Write-Host "Uninstalling DataFusion..."
-    $app.uninstall() Write-Host "Searching for existing Sysmon...."
-If (Get-WmiObject -Class Win32_Service -Filter "Name='Sysmon'") {
-    Write-Host "Uninstalling Sysmon..."
-    Start-Process -FilePath "Sysmon.exe" -Wait -ArgumentList "-u"
-}
-If (Get-WmiObject -Class Win32_Service -Filter "Name='Sysmon64'") {
-    Write-Host "Uninstalling Sysmon64..."
-    Start-Process -FilePath "Sysmon64.exe" -Wait -ArgumentList "-u"
-}
-
-Write-Host "Searching for existing Nxlog-CE..."
-$app = Get-WmiObject Win32_Product -filter "Name='NXLog-CE'"
-if($app) { 
-    Write-Host "Uninstalling Nxlog-CE..."
     $app.uninstall() 
-}
-
-Write-Host "Searching for existing OpenEDR (aka DataFusion)..."
-$app = Get-WmiObject Win32_Product -filter "Name='DataFusion'"
-if($app) { 
-    Write-Host "Uninstalling DataFusion..."
-    $app.uninstall() 
-}
-
-Stop-Process -erroraction 'silentlycontinue' -Force -Name "uat" | Out-Null
-Stop-Process -erroraction 'silentlycontinue' -Force -Name "dpfm" | Out-Null
-
-schtasks -erroraction 'silentlycontinue' /Delete /TN "UAT" /F | Out-Null
-schtasks -erroraction 'silentlycontinue' /Delete /TN "UATupload" /F | Out-Null 
-schtasks -erroraction 'silentlycontinue' /Delete /TN "DFPM" /F | Out-Null 
-
 }
 
 Stop-Process -erroraction 'silentlycontinue' -Force -Name "uat" | Out-Null
