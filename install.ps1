a6725e4dcb783613d6eed6b220178f8064e190b8
JC <jym@jym.sg>
2020-08-04 16:02:48 +0800

    additional checks

--- install.ps1
+++ install.ps1
@@ -1,3 +1,24 @@
+function Test-Administrator  
+{  
+    [OutputType([bool])]
+    param()
+    process {
+        [Security.Principal.WindowsPrincipal]$user = [Security.Principal.WindowsIdentity]::GetCurrent();
+        return $user.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator);
+    }
+}
+
+if(-not (Test-Administrator))
+{
+    Write-Error "This script must be executed as Administrator.";
+    exit;
+}
+
+if ($PSVersionTable.PSVersion.Major -lt 5)
+{
+    Write-Error "Need Powershell 5 or above.";
+}
+
 $TARGETDIR="c:\windows\openedr"
 $DOWNLOADDIR="$env:TEMP\openedr"
 $OPENEDRFILENAME='openedr.msi'
