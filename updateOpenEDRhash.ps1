6799014920ec15e5b93178e41520498a007dd2f2
JC <jymcheong@lol.com>
2021-07-21 10:39:29 +0800

    Delete updateOpenEDRhash.ps1

--- updateOpenEDRhash.ps1
+++
@@ -1,5 +0,0 @@
-$FileHash = Get-FileHash -Path openEDR.msi
-echo $FileHash.Hash
-(Get-Content -path install.ps1 -Raw) -match "OPENEDR_SHA256_HASH='(.*)'"
-$matches[1]
-((Get-Content -path install.ps1 -Raw) -replace $matches[1],$FileHash.Hash) | Set-Content -Path install.ps1
